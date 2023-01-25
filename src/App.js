4840b7dc098f66a32f6b51bb6f95536fe11dbb75
김지환 <91196025+Kimjipang@users.noreply.github.com>
2023-01-25 10:28:45 +0900

    [login], [router], [navbar], etc..

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
-  return;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
