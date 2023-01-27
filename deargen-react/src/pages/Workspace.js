07e3f601bf4a4a5a3cc1ac601c617dc73099c909
Kimjipang <91196025+Kimjipang@users.noreply.github.com>
2023-01-27 17:09:10 +0900

    Update Workspace.js

--- deargen-react/src/pages/Workspace.js
+++ deargen-react/src/pages/Workspace.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Workspace() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("access token");
+    if (!token) {
+      window.location.href("/loginver02");
+      // alert("접근할 수 없는 페이지입니다.");
+    }
+  }, []);
   return (
     <div>
-      <h1>Workspace</h1>
+      <h1>로그아웃 페이지</h1>
     </div>
   );
 }
