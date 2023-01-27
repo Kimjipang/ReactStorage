b460dcfe820f3dde0c1f65c8a4f939e00f458eca
Kimjipang <91196025+Kimjipang@users.noreply.github.com>
2023-01-27 17:08:51 +0900

    [Update Router]

--- deargen-react/src/App.js
+++ deargen-react/src/App.js
@@ -3,8 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import LoginVer02 from "./pages/LoginVer02";
-import LoginVer03 from "./pages/LoginVer03";
+import LoginVer02 from "./pages/Login";
 import Workspace from "./pages/Workspace";
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/loginver02" element={<LoginVer02 />} />
-          <Route path="/loginver03" element={<LoginVer03 />} />
           <Route path="/workspace" element={<Workspace />} />
         </Routes>
       </BrowserRouter>
