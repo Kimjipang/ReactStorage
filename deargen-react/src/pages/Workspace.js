27c44a6fed6f598be7d25de78c17f905f559dfc0
Kimjipang <91196025+Kimjipang@users.noreply.github.com>
2023-01-30 17:50:15 +0900

    [Workspace]

--- deargen-react/src/pages/Workspace.js
+++ deargen-react/src/pages/Workspace.js
@@ -1,20 +1,41 @@
 import React from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import Button from "@mui/material/Button";
+import Logout from "../features/logout";
+import "animate.css";
+import isConfirm from "../features/isConfirm";
 
 function Workspace() {
   const navigate = useNavigate();
+
+  // isConfirm();
+  const token = localStorage.getItem("access token");
+
   useEffect(() => {
-    const token = localStorage.getItem("access token");
     if (!token) {
-      window.location.href("/loginver02");
-      // alert("접근할 수 없는 페이지입니다.");
+      Swal.fire({
+        title: "로그인이 필요한 페이지입니다.",
+        text: "로그인 페이지로 이동합니다.",
+        icon: "warning",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login");
+        }
+      });
     }
   }, []);
+
   return (
-    <div>
-      <h1>로그아웃 페이지</h1>
-    </div>
+    token && (
+      <div>
+        <Button onClick={Logout} variant="outlined">
+          Logout
+        </Button>
+        <h1>Workspace</h1>
+      </div>
+    )
   );
 }
 
