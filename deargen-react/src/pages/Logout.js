095a61153215ba8135f4b513bb85b28f780317d3
Kimjipang <91196025+Kimjipang@users.noreply.github.com>
2023-01-30 17:41:05 +0900

    Delete Logout.js

--- deargen-react/src/pages/Logout.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import Button from "@mui/material/Button";
-
-function Logout() {
-  localStorage.removeItem("access token");
-}
-
-export default Logout;
