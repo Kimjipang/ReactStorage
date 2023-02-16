7fe1a56c8ab470a6c693f575bcdeb08463391d64
김지환 <91196025+Kimjipang@users.noreply.github.com>
2023-02-16 11:14:31 +0900

    Create Custompage.jsx

---
+++ src/pages/Custompage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CustomPage() {
+  return (
+    <body>
+      ddd
+      {/* <img src="https://api.time.com/wp-content/uploads/2022/11/GettyImages-1358149692.jpg"></img> */}
+    </body>
+  );
+}
