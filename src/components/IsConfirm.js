490c38eddf65edeabb02f20d24b4171e6fe6ba05
김지환 <91196025+Kimjipang@users.noreply.github.com>
2023-02-16 11:16:23 +0900

    [isConfirm] authorize

---
+++ src/components/IsConfirm.js
@@ -0,0 +1,19 @@
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+
+export default function IsConfirm() {
+  const token = localStorage.getItem("access token");
+  const navigate = useNavigate();
+
+  if (!token) {
+    Swal.fire({
+      title: "로그인이 필요한 페이지입니다.",
+      text: "로그인 페이지로 이동합니다.",
+      icon: "warning",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        navigate("/login");
+      }
+    });
+  }
+}
